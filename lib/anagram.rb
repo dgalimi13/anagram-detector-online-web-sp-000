488d8ff895bf61444662a63f2d5636591381f115
danny galimi <dannygalimi13@aim.com>
2020-09-29 21:13:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ end
 
 def match(array)
   new_array = []
-  self_split_sort =  self.split.sort
+  self_split_sort =  self.word.split.sort
  array.each do |element|
    if element.split.sort = self_split_sort
      new_array << element
